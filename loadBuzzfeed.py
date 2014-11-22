713ae497b0d515dd24829d023fdc2fe5649a9292
Jaren Glover <Jaren.G@Gmail.com>
2014-11-22 10:28:03 +0000

    refactor loading of file & confirm loading of big data set

--- loadBuzzfeed.py
+++ loadBuzzfeed.py
@@ -7,9 +7,9 @@ import psycopg2
 import argparse 
 import datetime as dt
 
-connection_string = "dbname='json_test' user='postgres' host='127.0.0.1' port='49192' password='postgres'"
+connection_string = "dbname='json_test' user='postgres' host='127.0.0.1' port='49153' password='postgres'"
 
-TABLE = 'daily'
+TABLE = 'test'
 COLUMN = 'data'
 
 def connect():
@@ -30,31 +30,34 @@ def parse_cmd():
   parser = argparse.ArgumentParser(description='Provide the file name and I will parse it')
   parser.add_argument('filename', type=argparse.FileType('r'),  help ='filename of the file to be chomped', nargs=1)
   args = parser.parse_args()
-  for line in args.filename:
-      FILE =line.readlines()
-  return FILE
+  for line in args.filename:  # <<<< prob not do this
+      #FILE =line.readlines()
+      return line
 
 def chomp(connection, cursor,document):
     '''
     '''
-    time_now = dt.datetime.now()
+    #time_now = dt.datetime.now()
     count = 0
     for line in document:
+        #print line
+	#sys.exit(187)
         SQL= "INSERT INTO %(table)s (%(column)s) VALUES ( \'%(data)s\' )" % {"table":TABLE, "column":COLUMN, "data":line.strip()}
         #print cursor.mogrify(SQL)
         cursor.execute(SQL)
-	
-	if count == 10000:
+	if count == 5000:
           connection.commit()
-          print "100 grand later" 
+          print "5K grand later" 
           count = 0
-      
+        count = count + 1 
     connection.commit()
     connection.close()
 
 if __name__ == "__main__":
     connection, cursor = connect()
     time_now = dt.datetime.now()
+    #print parse_cmd()
+    #sys.exit(187)
     chomp(connection,cursor,parse_cmd())
     time_later = dt.datetime.now()
     print "Time Delta: ", time_later - time_now
