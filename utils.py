583dbb88831101ecc53bcd285866eec7be37d2df
Jaren Glover <jaren.g@gmail.com>
2015-01-02 01:25:04 -0500

    added doc_string, remove test junk

--- utils.py
+++ utils.py
@@ -7,6 +7,14 @@ import json
 
 
 def json_to_list(json_string,strip_assets='',key=None):
+    '''
+
+    :param json_string: a single json string
+    :param strip_assets: chars you would like to be strip from json_string
+    :param key: the key the the values you would like to be parsed and mapped
+    :return: a list of floats from the json_string
+    '''
+
     #load string as json
     json_dic = json.loads(json_string)
 
@@ -17,12 +25,8 @@ def json_to_list(json_string,strip_assets='',key=None):
         #build a list of floats
         mapped_floats = [float(s) for s in dic_strip.split(',')]
     else:
-        raise KeyError("Your 'key' provided doesn't exists - please see function's docstring")
+        raise KeyError("The 'key' provided doesn't exists - please see function's docstring")
 
     return mapped_floats
 
 
-#test_string = '''{"hustle": {}, "name": "", "userid": 0.0, "numbers": [20.0, 30.0, 40.0,15.0, 65.0, 20.0],
-#  "nameid": 0.0, "user": "", "time": 87594, "query": {}, "sum": 456.0, "subname": 0.0}'''
-
-#print json_to_list(test_string,'[]','numbers')
